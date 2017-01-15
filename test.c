f9c02fc778d84ce18f38bfc2690e5117d0474b24
NicholasPetrielli <nicholas.petrielli@gmail.com>
2017-01-15 14:49:16 -0500

    Changes

--- test.c
+++ test.c
@@ -1,9 +1,59 @@
 #include <pocketsphinx.h>
 #include <time.h>
-int
-main(int argc, char *argv[])
+
+char* play_gofoward()
 {
-    ps_decoder_t *ps;
+	ps_decoder_t *ps;
+    cmd_ln_t *config;
+    FILE *fh;
+    char const *hyp, *uttid;
+    int16 buf[512];
+    int rv;
+    int32 score;
+
+    config = cmd_ln_init(NULL, ps_args(), TRUE,
+                 "-hmm", MODELDIR "/en-us/en-us",
+                 "-lm", MODELDIR "/en-us/en-us.lm.bin",
+                 "-dict", MODELDIR "/en-us/cmudict-en-us.dict",
+                 NULL);
+    if (config == NULL) {
+        fprintf(stderr, "Failed to create config object, see log for details\n");
+        return -1;
+    }
+    
+    ps = ps_init(config);
+    if (ps == NULL) {
+        fprintf(stderr, "Failed to create recognizer, see log for details\n");
+        return -1;
+    }
+
+    fh = fopen("goforward.raw", "rb");
+    if (fh == NULL) {
+        fprintf(stderr, "Unable to open input file goforward.raw\n");
+        return -1;
+    }
+
+    rv = ps_start_utt(ps);
+    
+    while (!feof(fh)) {
+        size_t nsamp;
+        nsamp = fread(buf, 2, 512, fh);
+        rv = ps_process_raw(ps, buf, nsamp, FALSE, FALSE);
+    }
+    
+    rv = ps_end_utt(ps);
+    hyp = ps_get_hyp(ps, &score);
+    printf("Recognized: %s\n", hyp);
+
+    fclose(fh);
+    ps_free(ps);
+    cmd_ln_free_r(config);
+	
+	return hyp;
+}
+
+void try_to_record(){
+	ps_decoder_t *ps;
     cmd_ln_t *config;
     FILE *fh;
     char const *hyp, *uttid;
@@ -12,7 +62,6 @@ main(int argc, char *argv[])
     int32 score;
     int16* bufWrite[512];
     int32* sizeWrite;
- //   *sizeWrite = 2;
     config = cmd_ln_init(NULL, ps_args(), TRUE,
                  "-hmm", MODELDIR "/en-us/en-us",
                  "-lm", MODELDIR "/en-us/en-us.lm.bin",
@@ -68,6 +117,11 @@ main(int argc, char *argv[])
     fclose(fh);
     ps_free(ps);
     cmd_ln_free_r(config);
-    
+}
+
+int
+main(int argc, char *argv[])
+{
+    printf("Returned: %s\n", play_gofoward());
     return 0;
 }
