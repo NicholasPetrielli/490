a19c2e78ae9d334b699a47d0c8af252b7ee52ac1
Nicholas Petrielli <nicholas.petrielli@gmail.com>
2017-02-05 23:10:14 -0500

    Now gets the weather properly

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
 static ps_decoder_t *ps;
 static cmd_ln_t *config;
@@ -51,6 +52,56 @@ sleep_msec(int32 ms)
 
     select(0, NULL, NULL, NULL, &tmo);
 #endif
+}
+
+void getWeather(){
+        system("curl \"http://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=279adcf563de661f5c8b7dd9dee187c5\" > file.txt");
+        system("tr ',' '\n' < file.txt >file2.txt");
+        system("tr -d '\"' < file2.txt > file.txt");
+        system("tr -d '[' < file.txt > file2.txt");
+        system("tr -d ']' < file2.txt > file.txt");
+        system("tr -d '{' < file.txt > file2.txt");
+        system("tr -d '}' < file2.txt > file.txt");
+        system("tr ':' ' ' < file.txt > file2.txt");
+        system("rm file.txt");
+        FILE *fp;
+        char buff[255];
+        char buffCopy[255];
+        char commandBuffer[600];
+        system("> weather.txt");
+
+        fp = fopen("file2.txt", "r");
+
+        while(fgets(buff, 255, (FILE*)fp)){
+                strcpy(buffCopy,buff);
+                int i = 0;
+                while( i < 255){
+                if(buffCopy[i] == '\n'){
+                        buffCopy[i] ='\0';
+                }
+                i++;
+                }
+                char* token = strtok(buff, " ");
+                while(token){
+                if        (strcmp(token, "description") == 0){
+                        sprintf(commandBuffer, "echo %s >> weather.txt", buffCopy);
+                        system(commandBuffer);
+                } else if (strcmp(token, "humidity") == 0){
+                        sprintf(commandBuffer, "echo %s >> weather.txt", buffCopy);
+                        system(commandBuffer);
+                } else if (strcmp(token, "main") == 0){
+                        token = strtok(NULL, " ");
+                        if (strcmp(token, "temp") == 0){
+                                token = strtok(NULL, " ");
+                                float num = atof(token) - 273.15;
+                                sprintf(commandBuffer, "echo The temperature is %.1f degrees celsius >> weather.txt", num);
+                                system(commandBuffer);
+                        }
+                }
+                token = strtok(NULL, " ");
+                }
+        }
+        fclose(fp);
 }
 
 char const* play_goforward()
@@ -188,6 +239,8 @@ void check_command(const char *words){
 			system("espeak -f date.txt -s 135 2>/dev/null");
 			printf("Do time stuff\n");
 		} else if(strcmp(token,weatherVar) == 0){
+			getWeather();
+			system("espeak -f weather.txt -s 135 2>/dev/null");
 			printf("Do weather stuff\n");
 		} else if (strcmp(token,sportVar) == 0  || strcmp(token,sportsVar) == 0){
 			printf("Do sport stuff\n");
